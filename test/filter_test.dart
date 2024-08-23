34c6784038563204aeb4127f71f3bcf3c13e588a
kingwill101 <hey@glenfordwilliams.com>
2024-08-23 11:06:23 -0500

    date filters

--- test/filter_test.dart
+++ test/filter_test.dart
@@ -1,4 +1,16 @@
+import 'package:intl/intl.dart';
+import 'package:liquify/src/filters/array.dart';
+import 'package:liquify/src/filters/date.dart';
+import 'package:liquify/src/filters/html.dart' as html;
+import 'package:test/test.dart';
+import 'package:timezone/timezone.dart' as tz;
+
 void main() {
+  setUp(() {
+    ensureTimezonesInitialized();
+    tz.setLocalLocation(tz.getLocation('America/New_York'));
+  });
+
   group('Array Filters', () {
     test('join', () {
       expect(join([1, 2, 3], [', '], {}), equals('1, 2, 3'));
@@ -96,4 +108,38 @@ void main() {
     });
   });
 
+  group('Date Filters', () {
+    test('date filter', () {
+      expect(date('2023-05-15', ['yyyy-MM-dd'], {}), equals('2023-05-15'));
+      expect(date('2023-05-15', ['MMMM d, yyyy'], {}), equals('May 15, 2023'));
+      expect(date('now', ['yyyy-MM-dd'], {}),
+          equals(DateFormat('yyyy-MM-dd').format(tz.TZDateTime.now(tz.local))));
+    });
+
+    test('date_to_xmlschema filter', () {
+      expect(dateToXmlschema('2023-05-15', [], {}),
+          equals('2023-05-15T00:00:00.000-04:00'));
+    });
+
+    test('date_to_rfc822 filter', () {
+      expect(dateToRfc822('2023-05-15', [], {}),
+          equals('Mon, 15 May 2023 00:00:00 -0400'));
+    });
+
+    test('date_to_string filter', () {
+      expect(dateToString('2023-05-15', [], {}), equals('15 May 2023'));
+      expect(
+          dateToString('2023-05-15', ['ordinal'], {}), equals('15th May 2023'));
+      expect(dateToString('2023-05-15', ['ordinal', 'US'], {}),
+          equals('May 15th, 2023'));
+    });
+
+    test('date_to_long_string filter', () {
+      expect(dateToLongString('2023-05-15', [], {}), equals('15 May 2023'));
+      expect(dateToLongString('2023-05-15', ['ordinal'], {}),
+          equals('15th May 2023'));
+      expect(dateToLongString('2023-05-15', ['ordinal', 'US'], {}),
+          equals('May 15th, 2023'));
+    });
+  });
 }
