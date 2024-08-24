ea0ca780818f11f6f8d7144626de1e1a5ab00a0f
kingwill101 <hey@glenfordwilliams.com>
2024-08-24 06:14:55 -0500

    reuse testParser helper

--- test/grammar_test.dart
+++ test/grammar_test.dart
@@ -1,16 +1,9 @@
-import 'dart:convert';
-
 import 'package:liquify/src/ast.dart';
-import 'package:liquify/src/grammar/grammar.dart';
-import 'package:liquify/src/registry.dart';
-import 'package:liquify/src/util.dart';
-import 'package:petitparser/debug.dart';
-import 'package:petitparser/petitparser.dart';
 import 'package:test/test.dart';
 
-void main() {
-  registerBuiltIns();
+import 'shared.dart';
 
+void main() {
   group('Liquid Grammar Parser', () {
     test('Parses liquid tags', () {
       testParser('''
@@ -893,44 +886,4 @@ void main() {
       });
     });
   });
-}
-
-Result parse(String input, [bool enableTrace = false]) {
-  final parser = LiquidGrammar().build();
-  if (enableTrace) {
-    return trace(parser).parse(input);
-  } else {
-    return parser.parse(input);
-  }
-}
-
-void testParser(String source, void Function(Document document) testFunction) {
-  try {
-    final result = parse(source);
-    if (result is Success) {
-      final document = result.value as Document;
-      try {
-        testFunction(document);
-      } catch (e) {
-        print('Error: $e');
-        printAST(document, 0);
-
-        JsonEncoder encoder = JsonEncoder.withIndent('  ');
-        final encoded = encoder.convert(document);
-        print(encoded);
-
-        rethrow;
-      }
-    } else {
-      final lineAndColumn = Token.lineAndColumnOf(source, result.position);
-
-      fail(
-          'Parsing failed:  Error ${result.message} at ${lineAndColumn.join(':')}');
-    }
-  } catch (e, trace) {
-    print("source: $source");
-    print('Error: $e');
-    print('Trace: $trace');
-    rethrow;
-  }
 }
---
+++ test/shared.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+import 'package:liquify/parser.dart';
+import 'package:liquify/src/grammar/grammar.dart';
+import 'package:liquify/src/util.dart';
+import 'package:petitparser/debug.dart';
+import 'package:test/test.dart';
+
+Result parse(String input, [bool enableTrace = false]) {
+  final parser = LiquidGrammar().build();
+  if (enableTrace) {
+    return trace(parser).parse(input);
+  } else {
+    return parser.parse(input);
+  }
+}
+
+void testParser(String source, void Function(Document document) testFunction) {
+  registerBuiltIns();
+
+  try {
+    final result = parse(source);
+    if (result is Success) {
+      final document = result.value as Document;
+      try {
+        testFunction(document);
+      } catch (e) {
+        print('Error: $e');
+        printAST(document, 0);
+
+        JsonEncoder encoder = JsonEncoder.withIndent('  ');
+        final encoded = encoder.convert(document);
+        print(encoded);
+
+        rethrow;
+      }
+    } else {
+      final lineAndColumn = Token.lineAndColumnOf(source, result.position);
+
+      fail(
+          'Parsing failed:  Error ${result.message} at ${lineAndColumn.join(':')}');
+    }
+  } catch (e, trace) {
+    print("source: $source");
+    print('Error: $e');
+    print('Trace: $trace');
+    rethrow;
+  }
+}
