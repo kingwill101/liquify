ece0c930c95092a2b19c3929c4c907e30799ef21
kingwill101 <hey@glenfordwilliams.com>
2025-02-10 11:24:49 -0500

    case/when tag sync+async

--- test/tags/case_tag_test.dart
+++ test/tags/case_tag_test.dart
@@ -15,68 +15,136 @@ void main() {
     evaluator.context.clear();
   });
   group('Case/when tag', () {
-    test('case tag with single match', () {
-      testParser(
-          '{% assign handle = "cake" %}'
-          '{% case handle %}'
-          '{% when "cake" %}'
-          'This is a cake'
-          '{% when "cookie" %}'
-          'This is a cookie'
-          '{% else %}'
-          'This is not a cake nor a cookie'
-          '{% endcase %}',
-          (document) {
-        evaluator.evaluate(document);
-        expect(evaluator.buffer.toString().trim(), 'This is a cake');
+    group('sync evaluation', () {
+      test('case tag with single match', () async {
+        await testParser(
+            '{% assign handle = "cake" %}'
+            '{% case handle %}'
+            '{% when "cake" %}'
+            'This is a cake'
+            '{% when "cookie" %}'
+            'This is a cookie'
+            '{% else %}'
+            'This is not a cake nor a cookie'
+            '{% endcase %}', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(evaluator.buffer.toString().trim(), 'This is a cake');
+        });
+      });
+
+      test('case tag with multiple values in when', () async {
+        await testParser(
+            '{% assign handle = "biscuit" %}'
+            '{% case handle %}'
+            '{% when "cake" %}'
+            'This is a cake'
+            '{% when "cookie", "biscuit" %}'
+            'This is a cookie or biscuit'
+            '{% else %}'
+            'This is something else'
+            '{% endcase %}', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(
+              evaluator.buffer.toString().trim(), 'This is a cookie or biscuit');
+        });
+      });
+
+      test('case tag with else condition', () async {
+        await testParser(
+            '{% assign handle = "pie" %}'
+            '{% case handle %}'
+            '{% when "cake" %}'
+            'This is a cake'
+            '{% when "cookie" %}'
+            'This is a cookie'
+            '{% else %}'
+            'This is neither a cake nor a cookie'
+            '{% endcase %}', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(evaluator.buffer.toString().trim(),
+              'This is neither a cake nor a cookie');
+        });
+      });
+
+      test('case tag with no matching condition and no else', () async {
+        await testParser(
+            '{% assign handle = "pie" %}'
+            '{% case handle %}'
+            '{% when "cake" %}'
+            'This is a cake'
+            '{% when "cookie" %}'
+            'This is a cookie'
+            '{% endcase %}', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(evaluator.buffer.toString(), '');
+        });
       });
     });
-    test('case tag with multiple values in when', () {
-      testParser(
-          '{% assign handle = "biscuit" %}'
-          '{% case handle %}'
-          '{% when "cake" %}'
-          'This is a cake'
-          '{% when "cookie", "biscuit" %}'
-          'This is a cookie or biscuit'
-          '{% else %}'
-          'This is something else'
-          '{% endcase %}', (document) {
-        evaluator.evaluate(document);
-        expect(
-            evaluator.buffer.toString().trim(), 'This is a cookie or biscuit');
+
+    group('async evaluation', () {
+      test('case tag with single match', () async {
+        await testParser(
+            '{% assign handle = "cake" %}'
+            '{% case handle %}'
+            '{% when "cake" %}'
+            'This is a cake'
+            '{% when "cookie" %}'
+            'This is a cookie'
+            '{% else %}'
+            'This is not a cake nor a cookie'
+            '{% endcase %}', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(evaluator.buffer.toString().trim(), 'This is a cake');
+        });
       });
-    });
-  });
 
-  test('case tag with else condition', () {
-    testParser(
-        '{% assign handle = "pie" %}'
-        '{% case handle %}'
-        '{% when "cake" %}'
-        'This is a cake'
-        '{% when "cookie" %}'
-        'This is a cookie'
-        '{% else %}'
-        'This is neither a cake nor a cookie'
-        '{% endcase %}', (document) {
-      evaluator.evaluate(document);
-      expect(evaluator.buffer.toString().trim(),
-          'This is neither a cake nor a cookie');
-    });
-  });
+      test('case tag with multiple values in when', () async {
+        await testParser(
+            '{% assign handle = "biscuit" %}'
+            '{% case handle %}'
+            '{% when "cake" %}'
+            'This is a cake'
+            '{% when "cookie", "biscuit" %}'
+            'This is a cookie or biscuit'
+            '{% else %}'
+            'This is something else'
+            '{% endcase %}', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(
+              evaluator.buffer.toString().trim(), 'This is a cookie or biscuit');
+        });
+      });
 
-  test('case tag with no matching condition and no else', () {
-    testParser(
-        '{% assign handle = "pie" %}'
-        '{% case handle %}'
-        '{% when "cake" %}'
-        'This is a cake'
-        '{% when "cookie" %}'
-        'This is a cookie'
-        '{% endcase %}', (document) {
-      evaluator.evaluate(document);
-      expect(evaluator.buffer.toString(), '');
+      test('case tag with else condition', () async {
+        await testParser(
+            '{% assign handle = "pie" %}'
+            '{% case handle %}'
+            '{% when "cake" %}'
+            'This is a cake'
+            '{% when "cookie" %}'
+            'This is a cookie'
+            '{% else %}'
+            'This is neither a cake nor a cookie'
+            '{% endcase %}', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(evaluator.buffer.toString().trim(),
+              'This is neither a cake nor a cookie');
+        });
+      });
+
+      test('case tag with no matching condition and no else', () async {
+        await testParser(
+            '{% assign handle = "pie" %}'
+            '{% case handle %}'
+            '{% when "cake" %}'
+            'This is a cake'
+            '{% when "cookie" %}'
+            'This is a cookie'
+            '{% endcase %}', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(evaluator.buffer.toString(), '');
+        });
+      });
     });
   });
 }
