5a3c0739dfe940dc05aaab10340adbb24814f0fd
kingwill101 <hey@glenfordwilliams.com>
2025-02-10 15:11:34 -0500

    increment/decrement tag sync+async

--- test/tags/decrement_tag_test.dart
+++ test/tags/decrement_tag_test.dart
@@ -14,25 +14,137 @@ void main() {
   tearDown(() {
     evaluator.context.clear();
   });
-  group('Decrement Tag', () {
-    test('decrements variable', () {
-      testParser('''
-  {% decrement my_counter %}
-  {% decrement my_counter %}
-  {% decrement my_counter %}''', (document) {
-        evaluator.evaluate(document);
-        expect(evaluator.buffer.toString(), '-1\n-2\n-3');
+
+  group('DecrementTag', () {
+    group('sync evaluation', () {
+      test('starts from -1', () async {
+        await testParser('{% decrement counter %}', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(evaluator.buffer.toString(), '-1');
+        });
+      });
+
+      test('decrements sequentially', () async {
+        await testParser('''
+          {% decrement counter %}
+          {% decrement counter %}
+          {% decrement counter %}
+        ''', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '-1-2-3'
+          );
+        });
+      });
+
+      test('is independent from assign', () async {
+        await testParser('''
+          {% assign counter = 42 %}
+          {% decrement counter %}
+          {{ counter }}
+        ''', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '-142'
+          );
+        });
+      });
+
+      test('shares state with increment', () async {
+        await testParser('''
+          {% increment counter %}
+          {% decrement counter %}
+          {% increment counter %}
+        ''', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '0-10'
+          );
+        });
+      });
+
+      test('maintains separate counters', () async {
+        await testParser('''
+          {% decrement counter1 %}
+          {% decrement counter2 %}
+          {% decrement counter1 %}
+          {% decrement counter2 %}
+        ''', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '-1-1-2-2'
+          );
+        });
       });
     });
 
-    test('global variables are not affected by decrement', () {
-      testParser('''{% assign var = 10 %}
-    {% decrement var %}
-    {% decrement var %}
-    {% decrement var %}
-    {{ var }}''', (document) {
-        evaluator.evaluate(document);
-        expect(evaluator.buffer.toString(), '\n-1\n-2\n-3\n10');
+    group('async evaluation', () {
+      test('starts from -1', () async {
+        await testParser('{% decrement counter %}', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(evaluator.buffer.toString(), '-1');
+        });
+      });
+
+      test('decrements sequentially', () async {
+        await testParser('''
+          {% decrement counter %}
+          {% decrement counter %}
+          {% decrement counter %}
+        ''', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '-1-2-3'
+          );
+        });
+      });
+
+      test('is independent from assign', () async {
+        await testParser('''
+          {% assign counter = 42 %}
+          {% decrement counter %}
+          {{ counter }}
+        ''', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '-142'
+          );
+        });
+      });
+
+      test('shares state with increment', () async {
+        await testParser('''
+          {% increment counter %}
+          {% decrement counter %}
+          {% increment counter %}
+        ''', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '0-10'
+          );
+        });
+      });
+
+      test('maintains separate counters', () async {
+        await testParser('''
+          {% decrement counter1 %}
+          {% decrement counter2 %}
+          {% decrement counter1 %}
+          {% decrement counter2 %}
+        ''', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '-1-1-2-2'
+          );
+        });
       });
     });
   });
--- test/tags/increment_tag_test.dart
+++ test/tags/increment_tag_test.dart
@@ -14,22 +14,123 @@ void main() {
   tearDown(() {
     evaluator.context.clear();
   });
-  group('Increment Tag', () {
-    test('increments variable', () {
-      testParser(
-          '{% increment my_counter %}{% increment my_counter %}{% increment my_counter %}',
-          (document) {
-        evaluator.evaluate(document);
-        expect(evaluator.buffer.toString(), '012');
+
+  group('IncrementTag', () {
+    group('sync evaluation', () {
+      test('starts from 0 and increments', () async {
+        await testParser('''
+          {% increment counter %}
+          {% increment counter %}
+          {% increment counter %}
+        ''', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '012'
+          );
+        });
+      });
+
+      test('is independent from assign', () async {
+        await testParser('''
+          {% assign counter = 42 %}
+          {% increment counter %}
+          {{ counter }}
+        ''', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '042'
+          );
+        });
+      });
+
+      test('shares state with decrement', () async {
+        await testParser('''
+          {% increment counter %}
+          {% decrement counter %}
+          {% increment counter %}
+        ''', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '0-10'
+          );
+        });
+      });
+
+      test('maintains separate counters', () async {
+        await testParser('''
+          {% increment counter1 %}
+          {% increment counter2 %}
+          {% increment counter1 %}
+          {% increment counter2 %}
+        ''', (document) {
+          evaluator.evaluateNodes(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '0011'
+          );
+        });
       });
     });
 
-    test('global variables are not affected by increment', () {
-      testParser(
-          '{% assign var = 10 %}{% increment var %}{% increment var %}{% increment var %}{{ var }}',
-          (document) {
-        evaluator.evaluate(document);
-        expect(evaluator.buffer.toString(), '01210');
+    group('async evaluation', () {
+      test('starts from 0 and increments', () async {
+        await testParser('''
+          {% increment counter %}
+          {% increment counter %}
+          {% increment counter %}
+        ''', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '012'
+          );
+        });
+      });
+
+      test('is independent from assign', () async {
+        await testParser('''
+          {% assign counter = 42 %}
+          {% increment counter %}
+          {{ counter }}
+        ''', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '042'
+          );
+        });
+      });
+
+      test('shares state with decrement', () async {
+        await testParser('''
+          {% increment counter %}
+          {% decrement counter %}
+          {% increment counter %}
+        ''', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '0-10'
+          );
+        });
+      });
+
+      test('maintains separate counters', () async {
+        await testParser('''
+          {% increment counter1 %}
+          {% increment counter2 %}
+          {% increment counter1 %}
+          {% increment counter2 %}
+        ''', (document) async {
+          await evaluator.evaluateNodesAsync(document.children);
+          expect(
+            evaluator.buffer.toString().replaceAll(RegExp(r'\s+'), ''),
+            '0011'
+          );
+        });
       });
     });
   });
