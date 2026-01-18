540a787d0660a150570bc15356ee13c7c21b5ecd
kingwill101 <hey@glenfordwilliams.com>
2026-01-18 03:23:12 -0500

    Add TagDelimiterType to distinguish custom tag delimiter styles
    Custom tags can now declare whether they use {% %} (tag) or {{ }}
    (variable) delimiters via the delimiterType getter. This enables
    the parser to route custom tags to the correct parsing branch.
    
    Default is TagDelimiterType.tag for backwards compatibility.

--- pkgs/liquify/lib/src/mixins/parser.dart
+++ pkgs/liquify/lib/src/mixins/parser.dart
@@ -1,7 +1,22 @@
 import 'package:petitparser/petitparser.dart';
 
+/// Indicates the delimiter type used by a custom tag parser.
+enum TagDelimiterType {
+  /// Uses {% %} delimiters (standard tag syntax)
+  tag,
+
+  /// Uses {{ }} delimiters (variable-like syntax, e.g., {{ super() }})
+  variable,
+}
+
 mixin CustomTagParser {
   Parser parser() {
     return epsilon();
   }
+
+  /// The delimiter type this tag uses.
+  ///
+  /// Override this to return [TagDelimiterType.variable] if your custom tag
+  /// uses {{ }} syntax instead of {% %}.
+  TagDelimiterType get delimiterType => TagDelimiterType.tag;
 }
